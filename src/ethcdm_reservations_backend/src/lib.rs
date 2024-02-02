74df0921d055c5804bb680073616c60ffbd84f0d
Manuel Rivera <menu.rivera@gmail.com>
2024-02-02 17:59:07 -0600

    [ADD] -> definitions for state and methods at a basic level

--- src/ethcdm_reservations_backend/src/lib.rs
+++ src/ethcdm_reservations_backend/src/lib.rs
@@ -1,5 +1,9 @@
 mod types;
 
+use std::{cell::RefCell, collections::HashMap};
+
+use candid::Principal;
+
 use crate::types::*;
 
 #[ic_cdk::query]
@@ -7,6 +11,20 @@ fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
 
-// todo: create structs 
 // todo: define persistent state
+thread_local! {
+    static STATE: RefCell<State> =  RefCell::new(State {
+        tour_by_owner_id: HashMap::default(),
+        reservation_by_id: HashMap::default(),
+        reservations_by_owner_id: HashMap::default()
+    })
+}
+
 // todo: add methods
+fn register_tour(title: String, price: u32) {}
+fn create_reservation(tour_id: Principal, date: u32, hashed_password: String) {}
+fn validate_reservation(reservation_id: u32, password: String) {}
+fn list_tours() -> Vec<Tour> {
+    vec![]
+}
+fn cancel_reservation(reservation_id: u32) {}
--- src/ethcdm_reservations_backend/src/types.rs
+++ src/ethcdm_reservations_backend/src/types.rs
@@ -1,5 +1,14 @@
+use std::collections::HashMap;
+
 use crate::*;
 
+pub struct State {
+    pub tour_by_owner_id: HashMap<Principal, Tour>,
+    pub reservation_by_id: HashMap<String, Reservation>,
+    pub reservations_by_owner_id: HashMap<Principal, Vec<Reservation>>,
+}
+
+// todo: create structs
 pub struct Tour {}
 pub struct Reservation {}
 pub struct CancellationPolicy {}
