88d312850eef713c056b4daf80ace62d4ad18c83
Manuel Rivera <menu.rivera@gmail.com>
2024-02-02 17:41:42 -0600

    [ADD] -> types module

--- src/ethcdm_reservations_backend/src/lib.rs
+++ src/ethcdm_reservations_backend/src/lib.rs
@@ -1,4 +1,12 @@
+mod types;
+
+use crate::types::*;
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+// todo: create structs 
+// todo: define persistent state
+// todo: add methods
---
+++ src/ethcdm_reservations_backend/src/types.rs
@@ -0,0 +1,5 @@
+use crate::*;
+
+pub struct Tour {}
+pub struct Reservation {}
+pub struct CancellationPolicy {}
