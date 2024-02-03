e464dcbfe196299caf9c6a1c95d147125e0c0143
Manuel Rivera <menu.rivera@gmail.com>
2024-02-02 18:07:00 -0600

    [ADD] -> inflated the structs with fields

--- src/ethcdm_reservations_backend/src/types.rs
+++ src/ethcdm_reservations_backend/src/types.rs
@@ -1,4 +1,6 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, u8};
+
+use candid::CandidType;
 
 use crate::*;
 
@@ -8,7 +10,29 @@ pub struct State {
     pub reservations_by_owner_id: HashMap<Principal, Vec<Reservation>>,
 }
 
+// Miliseconds type to concise
+pub type Miliseconds = u32;
+
 // todo: create structs
-pub struct Tour {}
-pub struct Reservation {}
-pub struct CancellationPolicy {}
+#[derive(CandidType)]
+pub struct Tour {
+    title: String,
+    price: u32,
+}
+
+#[derive(CandidType)]
+pub struct Reservation {
+    owner: Principal,
+    tour_id: Principal,
+    reserved_date: Miliseconds,
+    hashed_password: String,
+    price_paid: u32,
+    name: String,
+    persons: u8,
+}
+
+#[derive(CandidType)]
+pub struct CancellationPolicy {
+    full_refuld: Miliseconds,
+    half_refuld: Miliseconds,
+}
