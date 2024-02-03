421f6faa016e9e9fc5152150e70454fe9088a7a9
Manuel Rivera <menu.rivera@gmail.com>
2024-02-02 18:13:02 -0600

    [ADD] -> types to be concise

--- src/ethcdm_reservations_backend/src/types.rs
+++ src/ethcdm_reservations_backend/src/types.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, u8};
+use std::{collections::HashMap, u32, u8};
 
 use candid::CandidType;
 
@@ -10,14 +10,16 @@ pub struct State {
     pub reservations_by_owner_id: HashMap<Principal, Vec<Reservation>>,
 }
 
-// Miliseconds type to concise
+// types to be concise
 pub type Miliseconds = u32;
+pub type ReservationId = u32;
+pub type Balance = u32;
 
 // todo: create structs
 #[derive(CandidType)]
 pub struct Tour {
     title: String,
-    price: u32,
+    price: Balance,
 }
 
 #[derive(CandidType)]
@@ -26,7 +28,7 @@ pub struct Reservation {
     tour_id: Principal,
     reserved_date: Miliseconds,
     hashed_password: String,
-    price_paid: u32,
+    price_paid: Balance,
     name: String,
     persons: u8,
 }
